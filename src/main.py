a86de7bed1ae32a4182dbebe21ee30b982fd6378
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-01-18 14:43:12 -0500

    Play with Model in main.py.
    Collect and print names of first 50 songs in playlist.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
 import logging
 
+from controller.get_playlist import parse_playlist_dict
+from model import Model
 
+
+# TODO: Move functionality to controller after messing around
+# TODO: Add URI to Song objects
 def main():
     TEST_PLAYLIST_ID = '0MiNiiLm7aSYZGW5RV3kR5'
+
+    model = Model()
+    playlist = model.get_user_playlist(TEST_PLAYLIST_ID)
+    tracks = parse_playlist_dict(playlist)
+
+    print(len(tracks))
+    for song in tracks:
+        print(song.name)
 
 
 if __name__ == '__main__':
