f87cb2950dce0d1e32988f28e7e6d69da8ac4140
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-01-18 14:41:03 -0500

    Automatically collect authorization.
    Use environment variables to authorize automatically. Instances of Model will automatically gain the client object

--- src/model/model.py
+++ src/model/model.py
@@ -1,18 +1,18 @@
 import spotipy
 
 
+# TODO: Switch this from env variables to parameters when deploying
+def spotify_client() -> spotipy.Spotify:
+    """Authorize an API token using the user's credentials"""
+
+    oauth = spotipy.SpotifyOAuth()
+    access_token = oauth.get_access_token(as_dict=False)
+    return spotipy.Spotify(auth=access_token)
+
+
 class Model:
-    def __init__(self, client_id: str, client_secret: str, redirect_uri: str):
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.redirect_uri = redirect_uri
-
-    def spotify_client(self) -> spotipy.Spotify:
-        """Authorize an API token using the user's credentials"""
-
-        oauth = spotipy.SpotifyOAuth(self.client_id, self.client_secret, self.redirect_uri)
-        access_token = oauth.get_access_token(as_dict=False)
-        return spotipy.Spotify(auth=access_token)
+    def __init__(self):
+        self.client = spotify_client()
 
     def all_user_playlists(self) -> list[dict]:
         """Get each of the user's playlists"""
@@ -25,5 +25,4 @@ class Model:
         :param track_limit: The maximum number of items to retrieve
         :return: A dictionary of the playlist items
         """
-        client = self.spotify_client()
-        return client.playlist_items(playlist_id, limit=track_limit)
+        return self.client.playlist_items(playlist_id, limit=track_limit)
