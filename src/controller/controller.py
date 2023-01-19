0a9f3be9973a2cc6cc87b6b131be0187d2dee8b3
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-01-19 11:44:54 -0500

    Import Model and View

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -1,3 +1,8 @@
+from model import Model
+from view import View
+
+
 class Controller:
-    def __init__(self):
-        pass
+    def __init__(self, model: Model, view: View):
+        self.model = model
+        self.view = view
