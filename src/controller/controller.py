6ba1e20699af0a8d66a07b8517925ea951f86d73
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-01-20 10:02:41 -0500

    Add method to parse playlists in Controller.

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -1,8 +1,36 @@
+import logging
+
+from controller.song import Song
 from model import Model
 from view import View
 
 
 class Controller:
     def __init__(self, model: Model, view: View):
+        logging.info('Initializing Controller')
         self.model = model
         self.view = view
+
+    def parse_raw_playlist(self, playlist_id: str) -> list[Song]:
+        """
+        Convert the raw dictionary into a list of songs.
+
+        :param playlist_id: The identifier of the playlist.
+        :return: A list of song objects
+        """
+        # TODO: Add URI to Song objects
+        raw_playlist = self.model.playlist_by_id(playlist_id)
+        tracks: list = raw_playlist['items']
+
+        songs = []
+        for track in tracks:
+            track_info = track['track']
+
+            name = track_info['name']
+            album = track_info['album']
+
+            # collect all artists for a track
+            artists = [artist['name'] for artist in track_info['artists']]
+
+            songs.append(Song(name, album, artists))
+        return songs
