9ad66befb1e8bb9ba6df18ecacd407da907ff8a2
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-01-20 10:37:56 -0500

    Add title page widgets

--- src/view/view.py
+++ src/view/view.py
@@ -1,9 +1,21 @@
-import logging
+import tkinter as tk
+from tkinter import ttk
 
 
-class View:
-    def __init__(self):
-        logging.info('Instantiating View')
+class View(tk.Frame):
+    def __init__(self, root):
+        super().__init__(root)
+        self.root = root
 
-    def render_title_page(self):
+    def render_title_page(self, playlists: tuple[str, ...]):
         """Display the title page and its elements."""
+        title_label = tk.Label(self, text='Playlist Quiz')
+        question_radio = tk.Radiobutton(self, text='Questions')
+
+        # playlist selection
+        n = tk.StringVar(self)
+        playlist_select = ttk.Combobox(self, textvariable=n)
+        playlist_select['values'] = playlists
+        playlist_select.current()
+
+        start = tk.Button(self, text='Start Quiz')
