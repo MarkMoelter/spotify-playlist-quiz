eb24363b8a99d27784babf8dbd19df3ec4d6eb1f
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-01-20 10:06:00 -0500

    Add logging

--- src/view/view.py
+++ src/view/view.py
@@ -1,6 +1,9 @@
+import logging
+
+
 class View:
     def __init__(self):
-        pass
+        logging.info('Instantiating View')
 
     def render_title_page(self):
         """Display the title page and its elements."""
