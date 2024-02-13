e847a1e3388f92c3ade5cbaf2210995ae17f24f0
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2024-02-13 09:31:26 -0500

    feat: move secrets to env vars

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import logging
 
+from dotenv import load_dotenv
+
 from controllers import Controller
 from models import Model
 from views import View
@@ -7,17 +9,19 @@ from views import View
 
 def main():
     TEST_PLAYLIST_ID = '0MiNiiLm7aSYZGW5RV3kR5'
+    load_dotenv()
 
     model = Model()
-    view = View()
+    # view = View()
+    #
+    # controller = Controller(model, view)
+    # controller.start()
 
     tracks = model.parse_raw_playlist(TEST_PLAYLIST_ID)
 
-    # tracks = controllers.parse_raw_playlist(TEST_PLAYLIST_ID)
-    #
-    # print(len(tracks))
-    # for song in tracks[:5]:
-    #     print(song)
+    print(len(tracks))
+    for song in tracks[:5]:
+        print(song)
 
 
 if __name__ == '__main__':
