093e57a453d23e4a3c3c60ae744b2912bb4018db
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2024-06-19 12:37:16 -0400

    format: format files in project

--- tests/test_track.py
+++ tests/test_track.py
@@ -2,6 +2,6 @@ from src.models.track import Track
 
 
 def test_track():
-    test_track = Track(name='test_name', album='test_album', artists=['test_artist'])
+    test_track = Track(name="test_name", album="test_album", artists=["test_artist"])
 
     assert len(test_track.artists) == 1
