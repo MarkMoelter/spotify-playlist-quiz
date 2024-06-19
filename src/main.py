c5b107365cc36ed2b559893b96b7485fc6793f05
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2024-06-19 11:28:13 -0400

    feat: update logging to use yaml config

--- src/main.py
+++ src/main.py
@@ -1,21 +1,36 @@
+import logging.config
+import pathlib
 from pprint import pprint
 
+import yaml
 from dotenv import load_dotenv
 
 from src.controllers import Controller
-from src.logger_setup import logger_setup
 from src.models import Model
 from src.views import View
 
+logger = logging.getLogger("my_app")
+
+
+def setup_logging():
+    config_file = pathlib.Path("logging_configs/config.yaml")
+    with open(config_file) as f_in:
+        config = yaml.safe_load(f_in)
+    logging.config.dictConfig(config)
+
 
 def main():
+    setup_logging()
     load_dotenv()
 
     model = Model()
     view = View()
 
     controller = Controller(model, view)
+
+    logger.info("starting my_app")
     controller.start()
+    logger.info("closing my_app")
 
     tracks = model.parse_raw_playlist(list(model.user_playlists().values())[0])
 
@@ -24,5 +39,4 @@ def main():
 
 
 if __name__ == '__main__':
-    logger_setup("quiz")
     main()
